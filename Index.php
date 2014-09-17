35a9ae0dc669418485227d2113de191e7ff64752
jayendran <jshanmugam@g.harvard.edu>
2014-09-16 22:50:54 -0400

    updated index.php and added the images folder and image

--- Index.php
+++ Index.php
@@ -11,19 +11,81 @@
     <title>Starter Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    <!--link href="css/bootstrap.min.css" rel="stylesheet"-->
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+    <script src="JavaScript/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <!-- Custom styles for this template -->
+    <!--link href="css/carousel.css" rel="stylesheet"-->
+	<!--link href="css/cover.css" rel="stylesheet"-->
 </head>
 <body>
-<div class ="container">
+<h1>Jayendran Shanmugam</h1>
+<hr>
+
+<div class="col-md-5">
+<img class="featurette-image img-responsive img-thumbnail" src="Images/PA260027.jpg" alt="Generic placeholder image">
 </div>
+<div class="panel-body">
+  <table>
+	<tr>
+	<td> <p>Project2 URL's</p></td>
+	</tr>
+	<tr>
+	<td><p>LiveURL: <a href="P2.jayendran.me">P2.jayendran.me</a></p></td>
+	</tr>
+	<tr>  
+	<td><p>GITHUB URL: <a href="https://github.com/jayendranshan/P2">GithubP2</a></p> </td>
+	</tr>
+	<tr>
+	<td> <p>Project3 URL's</p></td>
+	</tr>
+	<tr>
+	<td><p>LiveURL: <a href="P3.jayendran.me">P3.jayendran.me</a></p></td>
+	</tr>
+	<tr>  
+	<td><p>GITHUB URL: <a href="https://github.com/jayendranshan/P3">GithubP3</a></p> </td>
+	</tr>
+	<tr>
+	<td> <p>Project4 URL's</p></td>
+	</tr>
+	<tr>
+	<td><p>LiveURL: <a href="P4.jayendran.me">P4.jayendran.me</a></p></td>
+	</tr>
+	<tr>  
+	<td><p>GITHUB URL: <a href="https://github.com/jayendranshan/P4">Githubp4</a></p> </td>
+	</tr>
+</table>
+   </div>
+</div>
+
+<div>
+In 2+ sentences describe who you are and what you do.
+ I am Jayendran shanmugam and i live in Framingham, MA. I am working as senior software engineer in EdenredUSA and i have bachelor degree in computer science engineering from Anna university, Chennai, India.
+
+Why are you taking this course?
+I am taking this class to learn new technologies like php, mysql. I am more interested in this course because most of the work will be done using command line.
+
+What previous experience do you have with programming?
+I have 8+ years of experience in working with microsoft web development technologies. I have mostly worked in developing web application and in maintaining application with ASP.net and c# as programming language. I have also worked with webservices, WCFServices, Javascript, Jquery, AJAX technologies, SQL server 2003, 2005, 2010, SSIS, SSRS. I have also have experience developing windows applications briefly.
+
+Are you working on a Mac, PC, or some other platform?
+I am working in PC.
+
+Anything else we should know?
+This is the first time i am studying in America and i am studying towards getting my masters in harvard.
+</div>
+
+<script src="JavaScript/jquery.min.js"></script>
+    <script src="JavaScript/bootstrap.min.js"></script>
+    <script src="JavaScript/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="JavaScript/ie10-viewport-bug-workaround.js"></script>
 </body>
 </html>
