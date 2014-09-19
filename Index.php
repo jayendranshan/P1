55ebeff8877d458b6c026dccf6acdbef479548c9
jayendran <jshanmugam@g.harvard.edu>
2014-09-18 23:21:06 -0400

    Updated index.php with CSS and added CSS file

--- Index.php
+++ Index.php
@@ -8,10 +8,10 @@
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
 
-    <title>Starter Template for Bootstrap</title>
+    <title>Project 1</title>
 
     <!-- Bootstrap core CSS -->
-    <!--link href="css/bootstrap.min.css" rel="stylesheet"-->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="JavaScript/ie-emulation-modes-warning.js"></script>
@@ -26,66 +26,82 @@
 	<!--link href="css/cover.css" rel="stylesheet"-->
 </head>
 <body>
-<h1>Jayendran Shanmugam</h1>
-<hr>
-
-<div class="col-md-5">
-<img class="featurette-image img-responsive img-thumbnail" src="Images/PA260027.jpg" alt="Generic placeholder image">
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+<div class="container">
+	<div class="navbar-header">
+        <font size="7" color="white">Jayendran Shanmugam</font>
+	</div>
 </div>
-<div class="panel-body">
-  <table>
-	<tr>
-	<td> <p>Project2 URL's</p></td>
-	</tr>
-	<tr>
-	<td><p>LiveURL: <a href="P2.jayendran.me">P2.jayendran.me</a></p></td>
-	</tr>
-	<tr>  
-	<td><p>GITHUB URL: <a href="https://github.com/jayendranshan/P2">GithubP2</a></p> </td>
-	</tr>
-	<tr>
-	<td> <p>Project3 URL's</p></td>
-	</tr>
-	<tr>
-	<td><p>LiveURL: <a href="P3.jayendran.me">P3.jayendran.me</a></p></td>
-	</tr>
-	<tr>  
-	<td><p>GITHUB URL: <a href="https://github.com/jayendranshan/P3">GithubP3</a></p> </td>
-	</tr>
-	<tr>
-	<td> <p>Project4 URL's</p></td>
-	</tr>
-	<tr>
-	<td><p>LiveURL: <a href="P4.jayendran.me">P4.jayendran.me</a></p></td>
-	</tr>
-	<tr>  
-	<td><p>GITHUB URL: <a href="https://github.com/jayendranshan/P4">Githubp4</a></p> </td>
-	</tr>
-</table>
+</div>
+<br />
+    <br />
+<!--div class="col-md-5"-->
+<div class="jumbotron">
+    <div class="container">
+        <table>
+            <tr>
+                <td>
+                    <div style="width:1000px">
+                        <div style="float:left;">
+                            <img class="img-rounded" src="Images/PA260027.jpg" width="600" height="365" alt="Generic placeholder image">
+                        </div>
+
+                        <div class="panel panel-info " style="width:380px;height:365px;float:right">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Project URL's</h3>
+                            </div>
+                            <div class="panel-body">
+                                <table>
+                                    <tr>
+                                        <td> <p>Project2 URL's</p></td>
+                                    </tr>
+                                    <tr>
+                                        <td>LiveURL: <a href="http://P2.jayendran.me">P2.jayendran.me</a>&nbsp; GithubURL: <a href="https://github.com/jayendranshan/P2">GithubP2</a></td>
+
+                                    </tr>
+                                    <tr><td><p></p></td></tr>
+                                    <tr>
+                                        <td> <p>Project3 URL's</p></td>
+                                    </tr>
+                                    <tr>
+                                        <td>LiveURL: <a href="http://P3.jayendran.me">P3.jayendran.me</a>&nbsp;GithubURL: <a href="https://github.com/jayendranshan/P3">GithubP3</a></td>
+                                    </tr>
+                                    <tr><td><p></p></td></tr>
+                                    <tr>
+                                        <td> <p>Project4 URL's</p></td>
+                                    </tr>
+                                    <tr>
+                                        <td>LiveURL: <a href="http://P4.jayendran.me">P4.jayendran.me</a>&nbsp;GithubURL: <a href="https://github.com/jayendranshan/P4">Githubp4</a></td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        </table>
+
+        <div class="alert alert-info" role="alert">
+            <strong>In 2+ sentences describe who you are and what you do?</strong><br /> This alert needs your attention, but it's not super important.
+            I am Jayendran shanmugam and i live in Framingham, MA. I am working as senior software engineer in EdenredUSA and
+            I have bachelor degree in computer science engineering from Anna university, Chennai, India. <br /><br />
+
+            <strong>Why are you taking this course?</strong><br />
+            I am taking this class to learn new technologies like php, mysql. I am more interested in this course because most of the work will be done using command line. <br /><br />
+
+            <strong>What previous experience do you have with programming?</strong><br />
+            I have 8+ years of experience in working with microsoft web development technologies. I have mostly worked in developing web application and in maintaining application with ASP.net and
+            c# as programming language. I have also worked with webservices, WCFServices, Javascript, Jquery, AJAX technologies, SQL server 2003, 2005, 2010, SSIS, SSRS.
+            I have also have experience developing windows applications briefly. <br /><br />
+
+            <strong>Are you working on a Mac, PC, or some other platform?</strong><br />
+            I am working in PC. <br /><br />
+
+            <strong>Anything else we should know?</strong><br />
+            This is the first time i am studying in America and i am studying towards getting my masters in harvard. <br /><br />
+        </div>
    </div>
-</div>
+ </div>
 
-<div>
-In 2+ sentences describe who you are and what you do.
- I am Jayendran shanmugam and i live in Framingham, MA. I am working as senior software engineer in EdenredUSA and i have bachelor degree in computer science engineering from Anna university, Chennai, India.
-
-Why are you taking this course?
-I am taking this class to learn new technologies like php, mysql. I am more interested in this course because most of the work will be done using command line.
-
-What previous experience do you have with programming?
-I have 8+ years of experience in working with microsoft web development technologies. I have mostly worked in developing web application and in maintaining application with ASP.net and c# as programming language. I have also worked with webservices, WCFServices, Javascript, Jquery, AJAX technologies, SQL server 2003, 2005, 2010, SSIS, SSRS. I have also have experience developing windows applications briefly.
-
-Are you working on a Mac, PC, or some other platform?
-I am working in PC.
-
-Anything else we should know?
-This is the first time i am studying in America and i am studying towards getting my masters in harvard.
-</div>
-
-<script src="JavaScript/jquery.min.js"></script>
-    <script src="JavaScript/bootstrap.min.js"></script>
-    <script src="JavaScript/docs.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="JavaScript/ie10-viewport-bug-workaround.js"></script>
 </body>
 </html>
